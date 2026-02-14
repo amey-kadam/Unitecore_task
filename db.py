1a047a9041f16107ddc471f1110f8e5290bc091d
Amey kadam <amey.p.kadam@gmail.com>
2026-02-15 01:04:58 +0530

    db

--- db.py
+++ db.py
@@ -5,5 +5,13 @@ from config import MONGO_URL, DB_NAME
 client = MongoClient(MONGO_URL, server_api=ServerApi("1"))
 db = client[DB_NAME]
 
+companies_collection = db["companies"]
+
+companies_collection.create_index("email", unique=True)
+companies_collection.create_index("mobile", unique=True)
+
 def get_db():
     return db
+
+def get_companies_collection():
+    return companies_collection
